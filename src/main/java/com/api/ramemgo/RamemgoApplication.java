84c80f6e825d555ecf02ee7de83b5f84a8d9e172
renanstopaJTP <124263852+renanstopaJTP@users.noreply.github.com>
2024-06-03 19:36:49 -0300

    feat: first commit

---
+++ src/main/java/com/api/ramemgo/RamemgoApplication.java
@@ -0,0 +1,13 @@
+package com.api.ramemgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamemgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamemgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/ramemgo/RamemgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ramemgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamemgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
