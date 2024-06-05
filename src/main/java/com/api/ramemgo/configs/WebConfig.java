6892b270ebcf99f0e890919fd2a6b6faff42708b
renanstopaJTP <124263852+renanstopaJTP@users.noreply.github.com>
2024-06-05 00:39:05 -0300

    feat: cors config

--- src/main/java/com/api/ramemgo/configs/WebConfig.java
+++ src/main/java/com/api/ramemgo/configs/WebConfig.java
@@ -3,7 +3,6 @@ package com.api.ramemgo.configs;
 import com.api.ramemgo.interceptors.ApiKeyInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -20,12 +19,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(apiKeyInterceptor).addPathPatterns("/**");
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowedMethods("*")
-                .exposedHeaders("*");
-    }
 }
