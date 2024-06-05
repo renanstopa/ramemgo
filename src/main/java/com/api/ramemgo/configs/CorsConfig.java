5d095dded5f1e2b4d02db9956eed8282da6bfad7
renanstopaJTP <124263852+renanstopaJTP@users.noreply.github.com>
2024-06-05 01:28:06 -0300

    feat: cors config

---
+++ src/main/java/com/api/ramemgo/configs/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.api.ramemgo.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOrigin("https://ramengo-renanstopa.azurewebsites.net");
+        config.addAllowedOrigin("https://tech.redventures.com.br");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.addExposedHeader("*");
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+
+}
