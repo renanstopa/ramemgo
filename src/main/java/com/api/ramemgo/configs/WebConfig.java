37a4597364c96af2e2a319d00b7840fd020e2802
renanstopaJTP <124263852+renanstopaJTP@users.noreply.github.com>
2024-06-05 00:50:02 -0300

    feat: cors config

--- src/main/java/com/api/ramemgo/configs/WebConfig.java
+++ src/main/java/com/api/ramemgo/configs/WebConfig.java
@@ -3,6 +3,7 @@ package com.api.ramemgo.configs;
 import com.api.ramemgo.interceptors.ApiKeyInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -19,4 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(apiKeyInterceptor).addPathPatterns("/**");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "https://ramengo-renanstopa.azurewebsites.net", "https://tech.redventures.com.br") // Substitua pelo domínio do seu site externo
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 }
